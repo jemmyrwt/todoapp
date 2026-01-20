246546eb44d551ff970ec5956298e53e25da66e5
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 21:05:38 +0530

    Update todoRoutes.js

--- Backend/routes/todoRoutes.js
+++ Backend/routes/todoRoutes.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+const router = express.Router();
+const todoCtrl = require('../controllers/todoController');
+const auth = require('../middleware/authMiddleware');
+
+// Sabhi routes ke peeche auth laga diya
+router.get('/', auth, todoCtrl.getTodos);
+router.post('/', auth, todoCtrl.createTodo);
+router.put('/:id', auth, todoCtrl.updateTodo);
+router.delete('/:id', auth, todoCtrl.deleteTodo);
+
+module.exports = router;
