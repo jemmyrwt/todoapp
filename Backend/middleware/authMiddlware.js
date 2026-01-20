c1077b4dea7965a261bce1c059b2f7ba6b7739cd
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 21:24:25 +0530

    Update authMiddlware.js

--- Backend/middleware/authMiddlware.js
+++ Backend/middleware/authMiddlware.js
@@ -1,13 +1,15 @@
-
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: "No token, authorization denied" });
+    const authHeader = req.header('Authorization');
+    if (!authHeader) return res.status(401).json({ error: "Access Denied. No token provided." });
+
+    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "Invalid token format." });
 
     try {
-        const decoded = jwt.verify(token.split(" ")[1], 'secret_key');
-        req.user = decoded; // Token se user ID mil jayegi
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
         next();
     } catch (err) {
         res.status(401).json({ error: "Token is not valid" });
