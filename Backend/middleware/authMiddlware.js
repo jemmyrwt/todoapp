f3bc3ea8a41453d4de2838bbea9512982052b25d
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 21:04:24 +0530

    Update authMiddlware.js

--- Backend/middleware/authMiddlware.js
+++ Backend/middleware/authMiddlware.js
@@ -1,1 +1,15 @@
 
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ error: "No token, authorization denied" });
+
+    try {
+        const decoded = jwt.verify(token.split(" ")[1], 'secret_key');
+        req.user = decoded; // Token se user ID mil jayegi
+        next();
+    } catch (err) {
+        res.status(401).json({ error: "Token is not valid" });
+    }
+};
