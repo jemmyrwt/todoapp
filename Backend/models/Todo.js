98e585ebb027394bf81aff0d9a7c519855b972a3
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 21:06:57 +0530

    Update Todo.js

--- Backend/models/Todo.js
+++ Backend/models/Todo.js
@@ -1,1 +1,13 @@
 
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    prio: { type: String, default: 'low' },
+    cat: { type: String, default: 'Work' },
+    date: { type: String, default: 'No Deadline' },
+    done: { type: Boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Todo', TodoSchema);
