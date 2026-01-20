d504216d6be00163b0544f53701951be59ca873a
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 21:01:29 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,1 +1,17 @@
 
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+// Password hashing
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) next();
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model('User', UserSchema);
